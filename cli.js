5f949ef1a3ff5cd34e0e6f2721457dd252638312
Dorian Marchal <marchal.dorian@gmail.com>
2019-08-25 02:26:50 +0200

    Add support for non-english locales in CLI (#4)

--- cli.js
+++ cli.js
@@ -2,13 +2,14 @@
 
 const lookup = require('.')
 const query = process.argv.slice(2)
+const locale = process.env.LOCALE
 
 async function main() {
   if (!query.length) {
-    console.log('Usage: wiktionary <term>')
+    console.log('Usage: (LOCALE=<locale>) wiktionary <term>')
     return
   }
-  const result = await lookup(query)
+  const result = await lookup(query, locale)
   if (result) {
     console.log(result.text)
   } else {
